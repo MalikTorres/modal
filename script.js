df6ce4ddba69eed269f2ab70a89fc63159570a02
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-09-05 23:59:03 -0400

    added overlay and close handlers

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function
     () {
     console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
